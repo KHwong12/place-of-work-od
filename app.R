2f6dead39386c2ccb7c358f659030d4d44286af2
Kenneth Wong <khwongk12@gmail.com>
2021-02-17 17:03:27 +0800

    Use tidyverse style linter

--- app.R
+++ app.R
@@ -28,7 +28,7 @@ ui <- fluidPage(
   # Theme options --------
   theme = shinytheme("flatly"),
   includeCSS("style.css"),
-  
+
   meta_social(
     .meta = meta(),
     title = "The great migration of workers",
@@ -41,87 +41,87 @@ ui <- fluidPage(
   # Application title and subtitle
   titlePanel(h1("The great migration of workers"), windowTitle = "The great migration of workers"),
   h3("Where do the workers working in your district live, and where do the workers living in your districrt work"),
-  
+
   br(),
-  
+
   htmltools::includeMarkdown("description_md/introduction.md"),
-  
+
   hr(),
-  
+
   p("To start, choose the group of workers you are interested in terms of living and working places. 
     By default, this application chooses all workers living in Hong Kong Island and working in Kowloon."),
-  
-  div(class = "sticky",
-    
+
+  div(
+    class = "sticky",
+
     fluidRow(
       h3("Choose your area of interest:")
     ),
-    
+
     fluidRow(
-      
+
       # Origin picker
       column(
         6,
-        
+
         pickerInput(
-            "residence_group",
-            label = "Place of residence",
-            choices = origin_choice_list,
-            selected = origin_df$node_ID[1:4], # c(0, 1, 2, 3)
-            multiple = TRUE,
-            options = list(
-                `actions-box` = TRUE,
-                `deselect-all-text` = "Unselect All",
-                `select-all-text` = "Select All",
-                `none-selected-text` = "Select Region(s)...",
-                `selected-text-format`= "count",
-                `count-selected-text` = "{0} regions choosed (on a total of {1})"
-            ),
-            choicesOpt = NULL,
-            width = NULL,
-            inline = FALSE
+          "residence_group",
+          label = "Place of residence",
+          choices = origin_choice_list,
+          selected = origin_df$node_ID[1:4], # c(0, 1, 2, 3)
+          multiple = TRUE,
+          options = list(
+            `actions-box` = TRUE,
+            `deselect-all-text` = "Unselect All",
+            `select-all-text` = "Select All",
+            `none-selected-text` = "Select Region(s)...",
+            `selected-text-format` = "count",
+            `count-selected-text` = "{0} regions choosed (on a total of {1})"
+          ),
+          choicesOpt = NULL,
+          width = NULL,
+          inline = FALSE
         )
-        
       ),
-        
-        column(
-          6,
-          
-          # Destination picker
-          pickerInput(
-            "work_group",
-            label = "Place of work",
-            choices = destination_choice_list,
-            selected = destination_df$node_ID[5:9], # 22 - 26 (Kowloon)
-            multiple = TRUE,
-            options = list(
-              `actions-box` = TRUE,
-              `deselect-all-text` = "Unselect All",
-              `select-all-text` = "Select All",
-              `none-selected-text` = "Select Region(s)...",
-              `selected-text-format`= "count",
-              `count-selected-text` = "{0} regions choosed (on a total of {1})"
-            ),
-            choicesOpt = NULL,
-            width = NULL,
-            inline = FALSE
+
+      column(
+        6,
+
+        # Destination picker
+        pickerInput(
+          "work_group",
+          label = "Place of work",
+          choices = destination_choice_list,
+          selected = destination_df$node_ID[5:9], # 22 - 26 (Kowloon)
+          multiple = TRUE,
+          options = list(
+            `actions-box` = TRUE,
+            `deselect-all-text` = "Unselect All",
+            `select-all-text` = "Select All",
+            `none-selected-text` = "Select Region(s)...",
+            `selected-text-format` = "count",
+            `count-selected-text` = "{0} regions choosed (on a total of {1})"
+          ),
+          choicesOpt = NULL,
+          width = NULL,
+          inline = FALSE
         )
       )
     )
   ),
 
   hr(),
-  
+
   htmltools::includeMarkdown("description_md/heatmap.md"),
 
   br(),
-  
+
   plotlyOutput("odHeatmap", height = "800px"),
 
   hr(),
-  
+
   htmltools::includeMarkdown("description_md/sankey.md"),
-  
+
   br(),
 
   # fluidRow(width = 12, plotlyOutput("sankey", height = "1200px"), align = "center"),
@@ -132,12 +132,12 @@ ui <- fluidPage(
   htmltools::includeMarkdown("description_md/sankey-overall.md"),
 
   plotOutput("alluPlot", height = "1200px"),
-  
+
   p("When you select only one origin and select all destinations (or vice versa), you could produce a highlighted 
     chart designated to your area of interest like below."),
-  
+
   img(src = "fromtoKCD.png", width = "100%"),
-  
+
   hr(),
 
   # Footer ---------------
@@ -155,7 +155,7 @@ server <- function(input, output, session, ...) {
   # to relay the height/width of the plot's container, we'll query this
   # session's client data http://shiny.rstudio.com/articles/client-data.html
   cdata <- session$clientData
-  
+
   MIN_SCREEN_WIDTH <- 300
   MAX_SCREEN_WIDTH <- 768
 
@@ -173,9 +173,11 @@ server <- function(input, output, session, ...) {
         # text = paste0("Place of Residence: ", place_of_residence, "\n",
         #               "Place of Work: ", place_of_work_sim, "\n",
         #               "Number of Workers: ", formattable::comma(N_workers, digits = 0)),
-        text = paste0("There are ", formattable::comma(N_workers, digits = 0), " workers \n",
-                      "living in ", "<b>", place_of_residence, "</b>", " and\n",
-                      "working in ", "<b>", place_of_work_sim, "</b>"),
+        text = paste0(
+          "There are ", formattable::comma(N_workers, digits = 0), " workers \n",
+          "living in ", "<b>", place_of_residence, "</b>", " and\n",
+          "working in ", "<b>", place_of_work_sim, "</b>"
+        ),
         fill = N_workers
       )
     ) +
@@ -197,24 +199,29 @@ server <- function(input, output, session, ...) {
 
     # workaround for transformed colour scale
     # https://stackoverflow.com/questions/54107531/ggplotly-mouse-values-while-using-a-log-transformed-color-scale
-    
+
     # width should be between 300 to max width
-    width_heatmap = ifelse(cdata$output_odHeatmap_width > MAX_SCREEN_WIDTH,
-                           MAX_SCREEN_WIDTH,
-                           # ensure minimum width is 300
-                           max(cdata$output_odHeatmap_width, MIN_SCREEN_WIDTH))
-    height_heatmap = width_heatmap * 18/22
-  
+    width_heatmap <- ifelse(cdata$output_odHeatmap_width > MAX_SCREEN_WIDTH,
+      MAX_SCREEN_WIDTH,
+      # ensure minimum width is 300
+      max(cdata$output_odHeatmap_width, MIN_SCREEN_WIDTH)
+    )
+    height_heatmap <- width_heatmap * 18 / 22
+
     # ggplotly cannot handle subtitle
     # https://datascott.com/blog/subtitles-with-ggplotly/
-    ggplotly(od_workers_plotly, tooltip = c("text"),
-             width = width_heatmap,
-             height = height_heatmap) %>%
-      layout(title = list(text = paste0('The OD of workers',
-                                        '<br>',
-                                        '<sup>',
-                                        'Number of workers living/working in every selected regions',
-                                        '</sup>')))
+    ggplotly(od_workers_plotly,
+      tooltip = c("text"),
+      width = width_heatmap,
+      height = height_heatmap
+    ) %>%
+      layout(title = list(text = paste0(
+        "The OD of workers",
+        "<br>",
+        "<sup>",
+        "Number of workers living/working in every selected regions",
+        "</sup>"
+      )))
   })
 
   output$sankey <- renderPlotly({
@@ -255,53 +262,65 @@ server <- function(input, output, session, ...) {
         hovertemplate = "There are %{value:.5d} workers<br />living in <b>%{source.label}</b> and<br />working in <b>%{target.label}</b>"
       )
     )
-    
+
     od_sankey_plotly <- od_sankey_plotly %>%
       layout(
         title = list(
-          text = paste0('Workers flowing',
-                        '<br>',
-                        '<sup>',
-                        'Flow of the commuters across the territory, classified by flows\' origin/destination',
-                        '</sup>')
+          text = paste0(
+            "Workers flowing",
+            "<br>",
+            "<sup>",
+            "Flow of the commuters across the territory, classified by flows' origin/destination",
+            "</sup>"
+          )
         )
-    )
+      )
 
     od_sankey_plotly
   })
-  
-  output$alluPlot <- renderPlot({
-    
+
+  output$alluPlot <- renderPlot(
+    {
       req(input$residence_group)
-      
+
       # manual gghighlight
       dataset_simlabel_selected_allu <- dataset_simlabel %>%
         mutate(
-          journey_type_colour_highlight = ifelse((origin_ID %in% input$residence_group & destination_ID %in% input$work_group), journey_type_colour, "#cccccc33"),
-          ) %>%
+          journey_type_colour_highlight = 
+            ifelse((origin_ID %in% input$residence_group & destination_ID %in% input$work_group),
+                   journey_type_colour,
+                   "#cccccc33"
+                   ),
+        ) %>%
         mutate(
           # put highlighted (selected) rows to the top and draw the non-highlighted grey flows first
-          journey_type_colour_highlight = factor(journey_type_colour_highlight, levels = c("#4daf4a66", "#377eb866", "#ff7f0066", "#984ea366", "#cccccc33"))
+          journey_type_colour_highlight = 
+            factor(journey_type_colour_highlight,
+                   levels = c("#4daf4a66", "#377eb866", "#ff7f0066", "#984ea366", "#cccccc33"))
         )
 
-      OD_work_alluvial_travelcolour <- 
-        ggplot(dataset_simlabel_selected_allu,
-               aes(y = N_workers, axis1 = place_of_residence, axis2 = place_of_work_sim, fill = journey_type_colour_highlight)) +
+      OD_work_alluvial_travelcolour <-
+        ggplot(
+          dataset_simlabel_selected_allu,
+          aes(y = N_workers, axis1 = place_of_residence, axis2 = place_of_work_sim, fill = journey_type_colour_highlight)
+        ) +
         geom_alluvium(aes(fill = journey_type_colour_highlight), width = .2, alpha = 1) +
         geom_stratum(width = .2, fill = "#EEEEEE", color = "#FFFFFF") +
         # http://corybrunson.github.io/ggalluvial/reference/stat_stratum.html
         # https://stackoverflow.com/questions/29465941/format-number-in-r-with-both-comma-thousands-separator-and-specified-decimals
-        geom_text(stat = "stratum",
-                  aes(label = paste0(after_stat(stratum),
-                                     # if count too small (i.e. short stratum height), keep district and count in one single line
-                                     ifelse(after_stat(count) < 1e5, " - ", "\n"),
-                                     # count with thousand separators
-                                     after_stat(format(count, big.mark = ",")))),
-                  size = 2) +
+        geom_text(
+          stat = "stratum",
+          aes(label = paste0(
+            after_stat(stratum),
+            # if count too small (i.e. short stratum height), keep district and count in one single line
+            ifelse(after_stat(count) < 1e5, " - ", "\n"),
+            # count with thousand separators
+            after_stat(format(count, big.mark = ","))
+          )),
+          size = 2
+        ) +
         scale_x_discrete(limits = c("Place of\nResidence", "Place of\nWork"), expand = c(.075, .075), position = "top") +
-        # scale_fill_manual(values = MA_NT_PALETTE, aesthetics = c("colour", "fill")) +
         scale_fill_identity() +
-        # scale_fill_brewer(type = "qual", palette = "Set1") +
         coord_cartesian(clip = "off") +
         theme_void() +
         theme(
@@ -317,7 +336,7 @@ server <- function(input, output, session, ...) {
           subtitle = "Place of residence and work of commuters in Hong Kong",
           caption = "Note: workers with no fixed places of work or \nwork in Marine/at home/in places outside Hong Kong are excluded."
         )
-      
+
       OD_work_alluvial_travelcolour
     },
     height = 1200,
